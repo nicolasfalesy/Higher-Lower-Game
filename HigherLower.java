a672601e2c155073857ab168bbb40a8eeb53d828
Nicolas <nfalesy@gmail.com>
2023-10-23 09:31:15 -0400

    Fixed bugs and addited functionality

--- HigherLower.java
+++ HigherLower.java
@@ -1,12 +1,3 @@
-/*
-* Name: Nicolas Falesy
-* Course: ICS3C
-* Teacher: Mrs. McCaffery
-* Date: 10/16/2023
-* Description: Tells user to guess a nubmer between 1-15 and tells them if they are right or wrong (They have 4 tries)
-* Inspiration: From Andrew Broderick, he's very helpful - He taught me how to use try/catch and while loops
-*/
-
 import java.util.*;
 
 public class HigherLower {
@@ -16,11 +7,12 @@ public class HigherLower {
         Scanner userInput = new Scanner(System.in);
         Random rand = new Random();
         int userNumber = 0;
-        int numGuesses = 4;
+        int numGuesses = 2;
         int rounds = 0;
+        int round = 1;
         int score = 0;
         int i = 1;
-        int round = 1;
+        boolean stillPlaying = true;
         // for printing messages
         int textSpeed = 22;
         int messageLongDelay = 1100;
@@ -29,7 +21,7 @@ public class HigherLower {
         // intro message for user (Explains the game)
         fancy_text("Welcome to my Higher Lower game!!", textSpeed, messageLongDelay);
         fancy_text("\nI'm thinking of a number between 1-15.", textSpeed, messageLongDelay);
-        fancy_text("\nYou have 4 tries to guess it.", textSpeed, messageLongDelay);
+        fancy_text("\nYou have " + numGuesses + " tries to guess it.", textSpeed, messageLongDelay);
         fancy_text("\nIf you guess the number correctly, you win!", textSpeed, messageLongDelay);
         fancy_text("\nIf you don't, I win!", textSpeed, messageLongDelay);
 
@@ -43,89 +35,90 @@ public class HigherLower {
         while (playing) {
             fancy_text("\nHow many rounds would you like to play?: ", textSpeed, messageLongDelay);
             // makes sure the user inputs a number
-            boolean isNumber = false;
-            while (!isNumber) {
-                isNumber = true;
-                try {
-                    rounds = userInput.nextInt();
-                } catch (InputMismatchException mismatch) {
-                    fancy_text("Please input an integer: ", textSpeed, messageShortDelay);
-                    userInput.nextLine();
-                    isNumber = false;
-                }
-            } // ends while
-              // checks if the number is valid
+            rounds = valid_number(userInput);
+            // checks if the number is valid
             if (rounds <= 0) {
-                fancy_text("Sorry, that number is not valid. Please try playing at a later time.", textSpeed,
+                fancy_text("Sorry, that is not valid. Please try playing at a later time.", textSpeed,
                         messageShortDelay);
-                System.exit(0);
+                stillPlaying = false;
             } else {
                 fancy_text("Okay, sounds good.", textSpeed, messageShortDelay);
             }
 
-            // loops through the rounds
-            for (round = 1; round <= rounds; round++) {
-                int randNum = rand.nextInt(15) + 1;
-                // asks user for the first number and stores it
-                fancy_text("\nROUND #" + round, textSpeed, messageShortDelay);
-                fancy_text("\nPlease enter your first guess between 1-15: ", textSpeed, messageShortDelay);
-
-                // the game
-                for (i = 1; i <= numGuesses; i++) {
-                    // makes sure the user inputs a number
-                    isNumber = false;
-                    while (!isNumber) {
-                        isNumber = true;
-                        try {
-                            userNumber = userInput.nextInt();
-                        } catch (InputMismatchException mismatch) {
-                            fancy_text("Please input an integer: ", textSpeed, messageShortDelay);
-                            userInput.nextLine();
-                            isNumber = false;
-                        }
-                    } // ends while
-
-                    // checks if the number is valid
-                    if (userNumber < 0 || userNumber > 15) {
-                        fancy_text("Sorry, that number is not valid. Please try playing at a later time.", textSpeed,
-                                messageLongDelay);
-                        System.exit(0);
-                        // checks if it is the number
-                    } else if (userNumber == randNum) {
-                        fancy_text("Awh man, you got it right! The number was infact " + randNum, textSpeed,
-                                messageLongDelay);
-                        score++;
-                        break;
-                        // checks if it is the first - third guess
-                    } else if (i < 4) {
-                        // checks if the number is too high
-                        if (userNumber > randNum) {
-                            fancy_text("Your number is too high, please try again: ", textSpeed, messageShortDelay);
-                            // checks if the number is too low
-                        } else if (userNumber < randNum) {
-                            fancy_text("Your number is too low, please try again!: ", textSpeed, messageShortDelay);
-                        }
-                        // checks if it is the last guess
+            while (stillPlaying) {
+                // loops through the rounds
+                for (round = 1; round <= rounds; round++) {
+                    int randNum = rand.nextInt(15) + 1;
+                    // a way for me to know approx what the number is (basically just
+                    // cheating/rigging it for myself lol)
+                    if (randNum <= 5) {
+                        fancy_text("\nThis number is going to be difficult!", textSpeed, messageLongDelay);
+                    } else if (randNum <= 10) {
+                        fancy_text("Don't worry, this will be a breeze!", textSpeed, randNum);
                     } else {
-                        fancy_text("Sorry, you ran out of tries. The number was " + randNum
-                                + "! That means I win, HAHAHA 😂😂", textSpeed, messageLongDelay);
-                        break;
+                        fancy_text("\nYou got this!", textSpeed, messageLongDelay);
                     }
-                } // ends for (game)
-            } // ends for (rounds)
+                    // asks user for the first number
+                    fancy_text("\nROUND #" + round, textSpeed, messageShortDelay);
+                    fancy_text("\nPlease enter your first guess between 1-15: ", textSpeed, messageShortDelay);
 
-            // prints out the user's score
-            double scorePercentage = score / rounds * 100;
-            fancy_text(
-                    "\nLooks like you got " + score + " out of " + rounds + " correct!\nThat's "
-                            + String.format("%.2f", scorePercentage) + "%!\nThanks for playing!",
-                    textSpeed, messageLongDelay);
-            playing = false;
+                    // the game
+                    for (i = 1; i <= numGuesses; i++) {
+                        // stores the number each round and checks if the number is valid
+                        boolean numberValid = false;
+                        while (!numberValid) {
+                            numberValid = true;
+                            userNumber = valid_number(userInput);
+                            if (userNumber <= 0 || userNumber > 15) {
+                                fancy_text("Sorry, that number is not valid. Please try again: ", textSpeed,
+                                        messageLongDelay);
+                                numberValid = false;
+                            } else {
+                                numberValid = true;
+                                break;
+                            }
+                        } // ends while (numberValid)
 
-            // Asks user to play again
+                        // checks if it is the number
+                        if (userNumber == randNum) {
+                            fancy_text("Awh man, you got it right! The number was infact " + randNum, textSpeed,
+                                    messageLongDelay);
+                            score++;
+                            break;
+                            // checks if it is the first - third guess
+                        } else if (i < numGuesses) {
+                            // checks if the number is too high
+                            if (userNumber > randNum) {
+                                fancy_text("Your number is too high, please try again: ", textSpeed, messageShortDelay);
+                                // checks if the number is too low
+                            } else if (userNumber < randNum) {
+                                fancy_text("Your number is too low, please try again!: ", textSpeed, messageShortDelay);
+                            }
+                            // checks if it is the last guess
+                        } else {
+                            fancy_text("Sorry, you ran out of tries. The number was " + randNum
+                                    + "! That means I win, HAHAHA 😂😂", textSpeed, messageLongDelay);
+                            break;
+                        }
+                    } // ends for (game)
+                } // ends for (rounds)
+
+                // calculates and prints out the user's score
+                double scorePercentage = (score / rounds) * 100;
+                fancy_text(
+                        "\nLooks like you got " + score + " out of " + rounds + " correct!",
+                        textSpeed, messageLongDelay);
+                fancy_text("\nThat's " + String.format("%.2f", scorePercentage) + "%!", textSpeed, messageShortDelay);
+                fancy_text("\nThanks for playing!", textSpeed, messageShortDelay);
+                playing = false;
+                stillPlaying = false;
+            } // ends while (stillPlaying)
+              // Asks user to play again
             fancy_text("\nWould you like to play again? (y/n): ", textSpeed, 0);
             String playingString = userInput.next();
             if (playingString.equalsIgnoreCase("yes") || playingString.equalsIgnoreCase("y")) {
+                // resets everything
+                stillPlaying = true;
                 playing = true;
                 score = 0;
                 round = 1;
@@ -133,6 +126,7 @@ public class HigherLower {
                 fancy_text("Ok, let's play again!", textSpeed, messageShortDelay);
             } else {
                 fancy_text("Ok, thanks for playing!", textSpeed, messageShortDelay);
+                System.exit(0);
             }
         } // ends while (playing)
         userInput.close();
@@ -146,4 +140,20 @@ public class HigherLower {
         }
         Thread.sleep(ms);
     } // ends fancy_text
+
+    public static int valid_number(Scanner scanner) {
+        int validNumber = 0;
+        boolean validInput = false;
+        while (!validInput) {
+            validInput = true;
+            try {
+                validNumber = scanner.nextInt();
+            } catch (InputMismatchException mismatch) {
+                System.out.println("Please input an integer: ");
+                scanner.nextLine();
+                validInput = false;
+            } // ends try/catch
+        }
+        return validNumber;
+    } // ends valid_number
 } // ends class
